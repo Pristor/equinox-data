7ebe3b51091011149d5c645d106a7029028a4886
Prister Patrick <patrick.prister00@gmail.com>
2021-01-10 17:56:03 +0100

    added POST and GET endpoint on /stations to create a new station and list all stations

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"database/sql"
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+	"log"
+	"net/http"
+)
+
+var (
+	psqlInfo = "host=localhost port=5432 user=postgres password=mysupercoolpsqlpassword database=equinox sslmode=disable"
+
+	config struct{
+		db *sql.DB
+	}
+)
+
+func init() {
+	var err error
+	config.db, err = sql.Open("postgres", psqlInfo)
+
+	if err != nil {
+		panic(err)
+	}
+
+	println("success")
+}
+
+func main()  {
+	log.Fatal(http.ListenAndServe(":8000", routes()))
+}
+
+func routes() *mux.Router {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/stations", handleNewStation).Methods(http.MethodPost)
+	r.HandleFunc("/stations", handleListStations).Methods(http.MethodGet)
+
+	return r
+}
